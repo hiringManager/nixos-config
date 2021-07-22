49d981eee369037ba06071728db4facfdd17b212
JD <64992493+hiringManager@users.noreply.github.com>
2021-07-22 11:56:06 -0500

    Add files via upload

---
+++ configuration.nix
@@ -0,0 +1,7 @@
+# Hosts file
+{
+  networking.extraHosts = let
+    hostsPath = https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts;
+    hostsFile = builtins.fetchurl hostsPath;
+  in builtins.readFile "${hostsFile}";
+}
